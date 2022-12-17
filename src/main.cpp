c1093c3571e5218c7820064f356d91220a31014a
ninaw04 <nina.wang@sjsu.edu>
2022-12-17 14:33:11 -0800

    Added angle clamping to MMT coauthored with Hanyu

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,11 @@
 
 int drive_pwm = 8;
 int drive_direction = 26;
-
+int angle_offset = 110;
 int input_speed = 0;
 int speed = 0;
 int steer_angle = 0;
+int steer_angle_clamped = 0;
 bool forward = true;
 
 Servo myservo;
@@ -73,6 +74,9 @@ void loop()
     Serial.println(actual_arguments);
     Serial.println(speed);
     Serial.println(steer_angle);
+    steer_angle_clamped = min(140, max(steer_angle + angle_offset, 90));
+    Serial.print("Clamped angle for servo is ");
+    Serial.println(steer_angle_clamped);
   }
 
   delay(100);
@@ -82,8 +86,8 @@ void loop()
   else {
     forward = false;
   }
-
+  
   speed = abs(input_speed);
   Move(forward, speed); // pass in true for forward, false for reverse
-  myservo.write(steer_angle);
+  myservo.write(steer_angle_clamped);
 }
